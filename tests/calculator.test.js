df537885f11ebb7cd3fc959222b913926cd27cba
nguyenn <goalduc@gmail.com>
2024-10-11 13:42:48 -0400

    [test] feature/tangent: added test cases

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -15,6 +15,7 @@ describe('Calculator Calculation Functions', function() {
     });
 });
 
+
 describe('Calculate reciprocal tests', function(){
     const res1 = calculator.calculateReciprocal(2);
     const res2 = () => calculator.calculateReciprocal(0);
@@ -24,7 +25,22 @@ describe('Calculate reciprocal tests', function(){
     it('should return an error', function() {})
         assert.throws(res2,expected);
 
-})
+});
 
+describe('Calculator tangent function', function(){
+    const res1 = calculator.tangent(0);
+    const res2 = calculator.tangent(Math.PI / 2);
+    const res3 = calculator.tangent(Math.PI / 4);
+    it('should return 0 if val is 0', function() {
+        assert.strictEqual(res1, 0);
+    });
+    
+    it('should return 16331239353195370 if val is Math.pi / 2'), function(){
+        assert.strictEqual(res2,16331239353195370);
+    }
 
+    it ('should return 0.9999999999999999 if val is Math.pi / 4'), function(){
+        assert.strictEqual(res3,0.9999999999999999);
+    }
+});
 // Add your tests here
