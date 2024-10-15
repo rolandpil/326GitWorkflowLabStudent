09a4a455a002a5ccd31167a6b6b64d88cbecf9eb
Kiet Nguyen (Kris) <kietnguyen.uma@gmail.com>
2024-10-15 16:09:44 -0400

    added cosine function and tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -8,6 +8,7 @@ describe('Calculator Memory Functions', function() {
     
 });
 
+describe("Calculator Memory Functions", function () {});
 
 // Test calculation functions
 describe("Calculator Calculation Functions", function () {
@@ -15,12 +16,15 @@ describe("Calculator Calculation Functions", function () {
   it("should return 2", function () {
     assert.strictEqual(res, 2);
   });
+<<<<<<< HEAD
 });
 describe("Calculator Calculation Functions", function () {
   const res = calculator.calculateSquareRoot(4);
   it("should return 2", function () {
     assert.strictEqual(res, 2);
   });
+=======
+>>>>>>> b8734ab (added cosine function and tests)
 });
 
 describe("Cosine Function", function () {
@@ -28,16 +32,16 @@ describe("Cosine Function", function () {
   it("should return 0.5", function () {
     assert.strictEqual(res, 0.5);
   });
-  const res2 = calculator.cosine(Math.PI/4)
-  it("should return 0", function() {
-    assert.strictEqual(res2, 0)
+  const res2 = calculator.cosine(Math.PI / 4);
+  it("should return 0", function () {
+    assert.strictEqual(res2, 0);
   });
-  const res3 = calculator.cosine(Math.PI/2)
-  it("should return -1", function() {
+  const res3 = calculator.cosine(Math.PI / 2);
+  it("should return -1", function () {
     assert.strictEqual(res3, -1);
   });
-  const res4 = calculator.cosine(Math.PI)
-  it("should return 1", function(){
+  const res4 = calculator.cosine(Math.PI);
+  it("should return 1", function () {
     assert.strictEqual(res4, 1);
   });
 });
